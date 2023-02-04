4b8f2d54b23b21abaa45f649f0649f380eeb2f94
Foucsi <julien.foucartreact@gmail.com>
2023-02-04 20:59:55 +0100

    mise en place de la rte post nouveau client coté back

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var facturesRouter = require("./routes/factures");
 var devisRouter = require("./routes/devis");
+var clientsRouter = require("./routes/clients");
 
 var app = express();
 const cors = require("cors");
@@ -24,5 +25,6 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/factures", facturesRouter);
 app.use("/devis", devisRouter);
+app.use("/clients", clientsRouter);
 
 module.exports = app;
--- routes/clients.js
+++ routes/clients.js
@@ -1,5 +1,29 @@
 var express = require("express");
 var router = express.Router();
 require("../models/connection");
+const { checkBody } = require("../modules/checkBody");
+
+const Clients = require("../models/clients");
+
+router.post("/", async (req, res) => {
+  const { name, tel, email, adress } = req.body;
+  if (!checkBody(req.body, ["name"])) {
+    return res.json({ result: false, error: "Missing or empty fields" });
+  }
+  const existingClients = await Clients.findOne({ name });
+  if (existingClients) {
+    res.jons({ result: false, error: "Clients deja existant!" });
+  }
+
+  const newClients = new Clients({
+    name,
+    tel,
+    email,
+    adress,
+  });
+
+  const saveNewClient = await newClients.save();
+  res.json({ result: true, client: saveNewClient });
+});
 
 module.exports = router;
