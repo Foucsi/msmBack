ee4cc209c4a894d6185aecf6da2dabd6128ef79d
Foucsi <julien.foucartreact@gmail.com>
2023-02-04 21:14:17 +0100

    creation de la rte clients/all coté back qui permet de recuperer tou les clients en db

--- routes/clients.js
+++ routes/clients.js
@@ -5,25 +5,38 @@ const { checkBody } = require("../modules/checkBody");
 
 const Clients = require("../models/clients");
 
+/* recuperer la liste des clients */
+router.get("/all", async (req, res) => {
+  const data = await Clients.find({});
+  if (!data) {
+    res.json({ result: false, message: "Pas de clients enregistré!" });
+  }
+  res.json({ result: true, data });
+});
+
+/* route permettant d'enregistrer un client en db */
 router.post("/", async (req, res) => {
   const { name, tel, email, adress } = req.body;
   if (!checkBody(req.body, ["name"])) {
     return res.json({ result: false, error: "Missing or empty fields" });
   }
-  const existingClients = await Clients.findOne({ name });
-  if (existingClients) {
-    res.jons({ result: false, error: "Clients deja existant!" });
+  try {
+    const existingClients = await Clients.findOne({ name });
+    if (existingClients) {
+      return res.json({ result: false, error: "Clients deja existant!" });
+    }
+    const newClients = new Clients({
+      name,
+      tel,
+      email,
+      adress,
+    });
+
+    const saveNewClient = await newClients.save();
+    res.json({ result: true, client: saveNewClient });
+  } catch (err) {
+    res.json({ result: false, error: err });
   }
-
-  const newClients = new Clients({
-    name,
-    tel,
-    email,
-    adress,
-  });
-
-  const saveNewClient = await newClients.save();
-  res.json({ result: true, client: saveNewClient });
 });
 
 module.exports = router;
