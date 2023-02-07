588f97af648a11ab49fd416d8b8ba9b24942c380
Foucsi <julien.foucartreact@gmail.com>
2023-02-07 16:10:07 +0100

    rte get sur un fichier dans compass a l'aide mongoclient.connect

--- routes/excel.js
+++ routes/excel.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var router = express.Router();
 require("../models/connection");
+var MongoClient = require("mongodb").MongoClient;
+const connectionString = process.env.CONNECTION_DB;
 
-const Excel = require("../models/excel");
+MongoClient.connect(connectionString, (err, client) => {
+  if (err) throw err;
+  const db = client.db("msm");
 
-router.get("/", (req, res) => {
-  Excel.find().then((data) => res.json({ result: true, data }));
+  router.get("/", (req, res) => {
+    db.collection("excel")
+      .find()
+      .toArray((err, data) => {
+        if (err) throw err;
+        res.json({ result: true, data });
+      });
+  });
 });
 
 module.exports = router;
