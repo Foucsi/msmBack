e947965dcc392e13606728cb7032ca62b0f4362d
Foucsi <julien.foucartreact@gmail.com>
2023-02-07 10:40:07 +0100

    commit

--- models/excel.js
+++ models/excel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const excelSchema = mongoose.Schema({
-  designation: String,
-  ref: String,
-  priceM: Number,
-  pricePanneau: Number,
-  type: String,
+  // designation: String,
+  // ref: String,
+  // priceM: Number,
+  // pricePanneau: Number,
+  // type: String,
 });
 
 const Excel = mongoose.model("excel", excelSchema);
--- routes/excel.js
+++ routes/excel.js
@@ -1,21 +1,11 @@
 var express = require("express");
 var router = express.Router();
-const XLSX = require("xlsx");
 require("../models/connection");
 
 const Excel = require("../models/excel");
 
-const workbook = XLSX.readFile("/Users/julienfoucart/Documents/msm.xlsx");
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const data = XLSX.utils.sheet_to_json(sheet);
-
-router.get("/", async (req, res) => {
-  try {
-    const data = await Excel.find();
-    res.json({ result: true, data });
-  } catch (err) {
-    res.json({ result: false, error: err });
-  }
+router.get("/", (req, res) => {
+  Excel.find().then((data) => res.json({ result: true, data }));
 });
 
 module.exports = router;
