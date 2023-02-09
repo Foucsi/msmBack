8a3053b8ac0f05c1a850f0b3d1b1be14b0be6d77
Foucsi <julien.foucartreact@gmail.com>
2023-02-09 11:13:06 +0100

    rte get by name

--- routes/clients.js
+++ routes/clients.js
@@ -8,10 +8,33 @@ const Clients = require("../models/clients");
 /* recuperer la liste des clients */
 router.get("/all", async (req, res) => {
   const data = await Clients.find({});
+
   if (!data) {
     res.json({ result: false, message: "Pas de clients enregistré!" });
   }
   res.json({ result: true, data });
+});
+
+/* recuperer info client byId */
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const client = await Clients.findOne({ _id: req.params.id });
+//     if (!client) {
+//       return res.status(404).json({ message: "Client not found" });
+//     }
+//     res.json({ result: true, client });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+/* recuperer les info client par son nom */
+router.get("/:name", async (req, res) => {
+  const name = req.params.name;
+  const client = await Clients.findOne({ name });
+  if (client) {
+    res.json({ result: true, client });
+  }
 });
 
 /* route permettant d'enregistrer un client en db */
